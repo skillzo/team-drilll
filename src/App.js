767ce98eb8d2471406fd0b7e059a60437f6a702f
skillzo <skillzfire@yahoo.com>
2022-11-19 09:45:49 +0100

    small fixes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
-import Section1 from "./Components/Section/Section1/Section1";
-import Section2 from "./Components/Section/Section2/Section2";
 import Section21 from "./Components/Section/Section2/Section2Outlets/section21/Section21";
 import Section22 from "./Components/Section/Section2/Section2Outlets/Section22/Section22";
 import Section23 from "./Components/Section/Section2/Section2Outlets/Section23/Section23";
-
-import SubNav from "./Components/SubNav/SubNav";
-import Wrapper from "./Components/UI/Wrapper/Wrapper";
-import Home from "./Pages/Home";
 import Main from "./Pages/Main";
 
 function App() {
